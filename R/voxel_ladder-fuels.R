b3a3ba28a46256ef5822eee97a621779d5e1be20
lisa <lisa@54973-DAR237.solar.sonoma.edu>
2020-12-09 10:16:09 -0800

    Add ALS and other kramer metrics

--- R/voxel_ladder-fuels.R
+++ R/voxel_ladder-fuels.R
@@ -51,14 +51,16 @@ library(glue)
 tls_las_folder <- 'D:/c1 - Pepperwood/c1_DEMnorm_las_plot'
 tls_las_files <- list.files(tls_las_folder, pattern = 'tls', full.names = TRUE)
 
-resolutions <- c(seq(0.01,0.05,0.01), seq(0.1,0.5,0.1), 1)
+als_las_folder <- 'D:/c1 - Pepperwood/c1_ALS_normplot'
+als_las_files <- list.files(als_las_folder, pattern = 'als', full.names = TRUE)
+
+res <- 0.1
 
 # out_file <- 'data/voxel_ladder-fuels.csv'
-out_file <- 'D:/Analyses/output/voxel_ladder-fuels.csv'
+out_file <- 'D:/Analyses/output/voxel_ladder-fuels_201208.csv'
 
 trad_file <- 'D:/Analyses/brie_thesis_parameters.csv'
 
-plot <- unique(combined_metrics$plot)
 
 fig_output <- 'D:/Analyses/figures'
 
@@ -67,9 +69,21 @@ fig_output <- 'D:/Analyses/figures'
 combined_metrics <- tibble(
   campaign = numeric(),
   plot = numeric(),
-  resolution_cm = numeric(),
-  voxel_ladder_fuel = numeric()
+  tls_voxel_ladder_fuel_1to2 = numeric(),
+  tls_voxel_ladder_fuel_2to3 = numeric(),
+  tls_voxel_ladder_fuel_3to4 = numeric(),
+  tls_voxel_ladder_fuel_2to4 = numeric(),
+  tls_voxel_ladder_fuel_1to4 = numeric(),
+  tls_voxel_ladder_fuel_1to8 = numeric(),
+  als_voxel_ladder_fuel_1to2 = numeric(),
+  als_voxel_ladder_fuel_2to3 = numeric(),
+  als_voxel_ladder_fuel_3to4 = numeric(),
+  als_voxel_ladder_fuel_2to4 = numeric(),
+  als_voxel_ladder_fuel_1to4 = numeric(),
+  als_voxel_ladder_fuel_1to8 = numeric()
+  
 )
+
 
 for (tls_file in tls_las_files) {
   
@@ -78,98 +92,223 @@ for (tls_file in tls_las_files) {
   plot <- str_extract(tls_file, '(?<=p)[:digit:]+') %>%
     as.numeric()
   
-  message('processing campaign ', campaign, ' plot ', plot)
+  message('processing TLS campaign ', campaign, ' plot ', plot)
   
   tls_las <- tls_file %>%
     readLAS(select = '')
 
-  for (res in resolutions) {
+  tls_plot_metric_a <- tls_las %>%
+    voxel_metrics( ~ length(Z), res) %>%
+    filter(Z <= 2) %>%
+    summarize(
+      tls_voxel_ladder_fuel_1to2 = sum(Z > 1)/n()
+    ) %>%
+    add_column(campaign, plot)
+  
+  tls_plot_metric_b <- tls_las %>%
+    voxel_metrics( ~ length(Z), res) %>%
+    filter(Z <= 3) %>%
+    summarize(
+      tls_voxel_ladder_fuel_2to3 = sum(Z > 2)/n()
+    ) 
+  
+  tls_plot_metric_c <- tls_las %>%
+    voxel_metrics( ~ length(Z), res) %>%
+    filter(Z <= 4) %>%
+    summarize(
+      tls_voxel_ladder_fuel_3to4 = sum(Z > 3)/n()
+    )
+  
+  tls_plot_metric_d <- tls_las %>%
+    voxel_metrics( ~ length(Z), res) %>%
+    filter(Z <= 4) %>%
+    summarize(
+      tls_voxel_ladder_fuel_2to4 = sum(Z > 2)/n()
+    ) 
     
-    message('processing resolution ', res * 100, ' cm')
-    
-    plot_metric <- tls_las %>%
-      voxel_metrics( ~ length(Z), res) %>%
-      filter(Z <= 4) %>%
-      summarize(
-        voxel_ladder_fuel = sum(Z > 1)/n()
-      ) %>%
-      add_column(campaign, plot, resolution_cm = res*100)
-    
-    combined_metrics <- combined_metrics %>%
-      add_row(plot_metric)
-  }
+  tls_plot_metric_e <- tls_las %>%
+    voxel_metrics( ~ length(Z), res) %>%
+    filter(Z <= 4) %>%
+    summarize(
+      tls_voxel_ladder_fuel_1to4 = sum(Z > 1)/n()
+    )
+  
+  tls_plot_metric_f <- tls_las %>%
+    voxel_metrics( ~ length(Z), res) %>%
+    filter(Z <= 8) %>%
+    summarize(
+      tls_voxel_ladder_fuel_1to8 = sum(Z > 1)/n()
+    ) 
+  
+  
+  
+
+  
+  tls_metrics <- bind_cols(tls_plot_metric_a, 
+            tls_plot_metric_b, 
+            tls_plot_metric_c, 
+            tls_plot_metric_d, 
+            tls_plot_metric_e, 
+            tls_plot_metric_f)
+  
 }
+
+for (als_file in als_las_files) {
+  
+  campaign <- str_extract(als_file, '(?<=c)[:digit:]') %>%
+    as.numeric()
+  plot <- str_extract(als_file, '(?<=p)[:digit:]+') %>%
+    as.numeric()
+  
+  message('processing ALS campaign ', campaign, ' plot ', plot)
+  
+  als_las <- als_file %>%
+    readLAS(select = '')
+  
+  als_plot_metric_a <- als_las %>%
+    voxel_metrics( ~ length(Z), res) %>%
+    filter(Z <= 2) %>%
+    summarize(
+      als_voxel_ladder_fuel_1to2 = sum(Z > 1)/n()
+    ) %>%
+    add_column(campaign, plot)
+  
+  als_plot_metric_b <- als_las %>%
+    voxel_metrics( ~ length(Z), res) %>%
+    filter(Z <= 3) %>%
+    summarize(
+      als_voxel_ladder_fuel_2to3 = sum(Z > 2)/n()
+    ) 
+  
+  als_plot_metric_c <- als_las %>%
+    voxel_metrics( ~ length(Z), res) %>%
+    filter(Z <= 4) %>%
+    summarize(
+      als_voxel_ladder_fuel_3to4 = sum(Z > 3)/n()
+    )
+  
+  als_plot_metric_d <- als_las %>%
+    voxel_metrics( ~ length(Z), res) %>%
+    filter(Z <= 4) %>%
+    summarize(
+      als_voxel_ladder_fuel_2to4 = sum(Z > 2)/n()
+    ) 
+  
+  als_plot_metric_e <- als_las %>%
+    voxel_metrics( ~ length(Z), res) %>%
+    filter(Z <= 4) %>%
+    summarize(
+      als_voxel_ladder_fuel_1to4 = sum(Z > 1)/n()
+    )
+  
+  als_plot_metric_f <- tls_las %>%
+    voxel_metrics( ~ length(Z), res) %>%
+    filter(Z <= 8) %>%
+    summarize(
+      als_voxel_ladder_fuel_1to8 = sum(Z > 1)/n()
+    ) 
+  
+  
+  
+  
+  
+  als_metrics <- bind_cols(als_plot_metric_a, 
+                           als_plot_metric_b,
+                           als_plot_metric_c, 
+                           als_plot_metric_d,
+                           als_plot_metric_e,
+                           als_plot_metric_f)
+  
+}
+  
+join <- left_join(tls_metrics, als_metrics, by=c('campaign','plot'))
+
+combined_metrics <- combined_metrics%>%
+  add_row(join)
+  
+
+
+
+
+  
+
+
 
  write.csv(combined_metrics, out_file)
 
-# ==============================================================================
 
-theme_set(
-  theme(text = element_text(family = 'serif', face = 'plain'),
-        axis.title = element_text(size = 16),
-        axis.text = element_text(size = 14),
-        line = element_line(size = 1),
-        axis.line = element_line(),
-        panel.background = element_rect(color = 'white'),
-        legend.title = element_text(size = 16),
-        legend.text = element_text(size = 14),
-        legend.key = element_blank(),
-        legend.spacing = unit(0, "cm"),
-        legend.margin = margin(0,5,0,5),
-        title = element_text(size = 12.8)
-  )
-)
+
  
-trad <- read_csv(trad_file) 
-
-
-for (i in plot) {
-  
-  subtrad <- subset(trad, plot==i)
-  ladder <- subtrad$trad_ladder_fuel
-  tls <- subtrad$tls_ladder_fuel
-  
-  figure <- ggplot(
-    data = subset(combined_metrics, plot %in% i) %>%
-      mutate_at('plot', as.factor),
-    mapping = aes(
-      x = resolution_cm,
-      y = voxel_ladder_fuel,
-      color = 'black'
-    )) +
-    geom_point() +
-    geom_line() + 
-    geom_hline(
-      mapping = aes(yintercept = ladder, 
-                    color = 'grey80'),
-      linetype = 'dashed',
-      size = 1
-    ) + 
-    geom_hline(
-      mapping = aes(yintercept = tls,
-                    color = 'firebrick'),
-      linetype = 'dashed',
-      size = 1
-    ) +
-    scale_color_identity(
-      name = glue('plot {i}'),
-      breaks = c('grey80', 'firebrick', 'black'),
-      labels = c('photo banner', 'TLS', 'tls voxels'),
-      guide = "legend"
-    )+
-    ylim(0,1)
-  
-  figure
-  
-  ggsave(
-    glue('{fig_output}/p{i}_voxel_ladder-fuels.png'),
-    width = 6.5,
-    height = 4.5,
-    units = 'in',
-    dpi = 700
-    
-  )
-  
-}
-
-
+ 
+# ==============================================================================
+# 
+# theme_set(
+#   theme(text = element_text(family = 'serif', face = 'plain'),
+#         axis.title = element_text(size = 16),
+#         axis.text = element_text(size = 14),
+#         line = element_line(size = 1),
+#         axis.line = element_line(),
+#         panel.background = element_rect(color = 'white'),
+#         legend.title = element_text(size = 16),
+#         legend.text = element_text(size = 14),
+#         legend.key = element_blank(),
+#         legend.spacing = unit(0, "cm"),
+#         legend.margin = margin(0,5,0,5),
+#         title = element_text(size = 12.8)
+#   )
+# )
+#  
+# plot <- unique(combined_metrics$plot) 
+# trad <- read_csv(trad_file) 
+# 
+# 
+# for (i in plot) {
+#   
+#   subtrad <- subset(trad, plot==i)
+#   ladder <- subtrad$trad_ladder_fuel
+#   tls <- subtrad$tls_ladder_fuel
+#   
+#   figure <- ggplot(
+#     data = subset(combined_metrics, plot %in% i) %>%
+#       mutate_at('plot', as.factor),
+#     mapping = aes(
+#       x = resolution_cm,
+#       y = voxel_ladder_fuel,
+#       color = 'black'
+#     )) +
+#     geom_point() +
+#     geom_line() + 
+#     geom_hline(
+#       mapping = aes(yintercept = ladder, 
+#                     color = 'grey80'),
+#       linetype = 'dashed',
+#       size = 1
+#     ) + 
+#     geom_hline(
+#       mapping = aes(yintercept = tls,
+#                     color = 'firebrick'),
+#       linetype = 'dashed',
+#       size = 1
+#     ) +
+#     scale_color_identity(
+#       name = glue('plot {i}'),
+#       breaks = c('grey80', 'firebrick', 'black'),
+#       labels = c('photo banner', 'TLS', 'tls voxels'),
+#       guide = "legend"
+#     )+
+#     ylim(0,1)
+#   
+#   figure
+#   
+#   ggsave(
+#     glue('{fig_output}/p{i}_voxel_ladder-fuels.png'),
+#     width = 6.5,
+#     height = 4.5,
+#     units = 'in',
+#     dpi = 700
+#     
+#   )
+#   
+# }
+# 
+# 
